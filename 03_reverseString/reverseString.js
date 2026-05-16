deff5fe6adbc1e8ddd3f9c5632ed31fcd36459fd
Diegogc29 <diego30stm@hotmail.com>
2026-05-16 14:21:59 -0600

    ejercicio resuelto

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let reversePhrase= ""
+    for (i=phrase.length;i>0;i--){
+      reversePhrase += phrase.slice(i-1,i); 
+    }
+    
+return reversePhrase;
 };
 
 // Do not edit below this line
