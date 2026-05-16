eb13d589f4e25fae9c737656ad3ba200e68ad98b
Diegogc29 <diego30stm@hotmail.com>
2026-05-16 13:22:07 -0600

    ejercicio resuelto

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(word, times) {
+let string= "";
+if (times<0){
+    return ("ERROR");
+}
+for (let i= 0; i < times; i++) {
+    console.log(word);
+    string += word;
+    console.log(string);
+}
+return string;
+}; 
 // Do not edit below this line
 module.exports = repeatString;
