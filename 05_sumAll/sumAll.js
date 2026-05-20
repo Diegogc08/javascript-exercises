6d17539f3ec12d9e9cc5853d0393032d9e841e0e
Diegogc29 <diego30stm@hotmail.com>
2026-05-20 14:23:07 -0600

    ejercicio resuelto

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numMin,numMax) {
+if (numMin < 0){
+    return "ERROR"
+}
+if (typeof numMin !=="number"||typeof numMax !=="number"){
+    return "ERROR"
+}
+if (!Number.isInteger (numMin)|| !Number.isInteger (numMax)) {
+    return "ERROR"
+}
+const min = Math.min(numMin, numMax);
+const max = Math.max(numMin, numMax);
 
+let suma = 0;
+
+for (let i = min; i <= max; i++){
+    suma += i;
+
+}
+return suma;
 };
 
 // Do not edit below this line
